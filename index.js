b2ad9468a949283f75a68f690bf9fdea45542cd9
priyanshu11223344 <priyanshupanchal2105@gmail.com>
2026-03-22 23:41:17 +0530

    added frontend src

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors=require("cors")
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "https://exam-frontend-iota.vercel.app/",
       credentials: true,
     })
   );
